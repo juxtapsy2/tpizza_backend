cd150c0dc1350f483b339f45a14a6af2b207c3e3
juxtapsy2 <frost.death.ap@gmail.com>
2025-04-28 18:28:43 +0700

    Fix mail sending issue

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587, // currently TSL, use 465 for SSL.
-  secure: false, // true if using 465
+  host: 'smtp.gmail.com',
+  port: 587, // TLS
+  secure: false, // true for SSL (465)
   auth: {
-    user: process.env.BREVO_EMAIL,
-    pass: process.env.BREVO_SMTP_KEY,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
 });
 
